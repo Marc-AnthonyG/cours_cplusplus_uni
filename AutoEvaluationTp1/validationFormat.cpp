ddc264c48ca344b1ca04094dad6a45ebeeb6d9e2
Marc-Anthony <78452236+Marc-AnthonyG@users.noreply.github.com>
2021-09-29 20:14:52 -0400

    commentaire début

--- AutoEvaluationTp1/validationFormat.cpp
+++ AutoEvaluationTp1/validationFormat.cpp
@@ -19,23 +19,30 @@
 using namespace std;
 
 
+/*
+ * ValiderFormatNon fonctionne de manière très simple. Il assume que le nom est valide,
+ puis il passe à travers tout les caractères. En fesant cela, il y a trois possibilité.
+ * (1) Le caractères est une lettre. Si c'est vrai, on ne teste rien d'autre et on passe au suivant.
+ * (2) Le caractères est un espace ou un tiret. Dans ce cas, le code vérifie que le caractère suivant est  un autre espace ou un tiret. Si ce n'est pas le cas on passe au caractère suivant sinon on considère le nom invalide.
+ * (3) Ce n'est pas un caractère valide donc on sort de l'ittération
+ */
 bool
 validerFormatNom (const string& p_nom)
 {
   bool isValid = true;
 
-  for (int i = 0; i < p_nom.length () && isValid; i++)
+  for (int i = 0; i < p_nom.length () && isValid; i++) //on rajoute isValid pour sortir de la boucle dès que le nom est invalide
     {
-      if (!isalpha (p_nom[i]))
+      if (!isalpha (p_nom[i])) //(1)
         {
-          if (p_nom[i] == '-' || p_nom[i] == ' ')
+          if (p_nom[i] == '-' || p_nom[i] == ' ') //(2)
             {
               if (i + 1 != p_nom.length ())
                 {
                   isValid = !(p_nom[i + 1] == '-' || p_nom[i + 1] == ' ');
                 }
             }
-          else
+          else //(3)
             {
               isValid = false;
             }
@@ -45,11 +52,14 @@ validerFormatNom (const string& p_nom)
 }
 
 
+/*
+ * isStringDigit passe à travers une string et vérifie si tout les termes de la string sont des nombres
+ */
 bool
 isStringDigit (const string& aValider)
 {
   bool isValid = true;
-  for (int i = 0; i < aValider.length () && isValid; i++)
+  for (int i = 0; i < aValider.length () && isValid; i++) // On rajoute isValid pour sortir dès que caractères n'est pas un nombre
     {
       if (!isdigit (aValider[i]))
         {
@@ -60,6 +70,11 @@ isStringDigit (const string& aValider)
 }
 
 
+/*
+ * isEditorValid Utilise les deux premiers caractères de l'identifiant de l'éditeur pour vérifier s'il est conforme.
+ *
+ * NOTE AU CORRECTEUR : Je ne suis pas sûr, mais je ne pense pas que les nombres de longeur 1 sont accepter pour l'éditeur. Pourtant dans les tests on dit que 2 est valide alors que je pense qu'il devrait plutôt être écrit 02.
+ */
 bool
 isEditorValid (const string& aValider)
 {
@@ -97,25 +112,19 @@ isEditorValid (const string& aValider)
 }
 
 
+/*
+ * isDomaineValid Utilise les trois premiers caractères de l'identifiant du domaine pour vérifier s'il est conforme.
+ *
+ */
 bool
 isDomaineValid (const string & aValider)
 {
   bool isValid = false;
-  int longeur = aValider.length ();
-  char * firstChar = new char[1];
-  strcpy (firstChar, aValider.substr (0, 1).c_str ());
-
-  char * secondChar = new char[1];
-  char * thirdChar = new char[1];
-  if (longeur >= 2)
-    {
-      strcpy (secondChar, aValider.substr (1, 1).c_str ());
-    }
-  if (longeur >= 4)
-    {
-      strcpy (thirdChar, aValider.substr (1, 1).c_str ());
-    }
-  switch (longeur)
+  //création de char pour la fonction atoi
+  char * firstChar =   strcpy (firstChar, aValider.substr (0, 1).c_str ());
+  char * secondChar = strcpy (secondChar, aValider.substr (1, 1).c_str ());
+  char * thirdChar = strcpy (thirdChar, aValider.substr (1, 1).c_str ());
+  switch (aValider.length ())
     {
       case 1:
         isValid = atoi (firstChar) <= 5 || atoi (firstChar) == 7;
